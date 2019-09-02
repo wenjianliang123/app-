6a52c64bd1670164573604a6b3d9cd21fac9c72e
wenjianliang <1321805139@qq.com>
2019-09-02 19:31:38 +0800

    第一次正式能够使用的提交

---
+++ app/Http/Controllers/wjl/ceshi.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\wjl;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ceshi extends Controller
+{
+    public function index()
+    {
+        echo "路由正确";
+    }
+}
